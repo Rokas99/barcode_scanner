c51dc87ea0e028d92dea32078d137e4022576621
Rokas99 <rok.amb99@gmail.com>
2023-04-24 17:19:36 +0300

    Init

---
+++ app/src/main/java/eurofondas/barcode_scanner/logic/ImageAnalyzer.kt
@@ -0,0 +1,47 @@
+import android.annotation.SuppressLint
+import android.content.Context
+import android.util.Log
+import android.widget.Toast
+import androidx.camera.core.ImageAnalysis
+import androidx.camera.core.ImageProxy
+import com.google.mlkit.vision.barcode.BarcodeScannerOptions
+import com.google.mlkit.vision.barcode.BarcodeScanning
+import com.google.mlkit.vision.barcode.common.Barcode
+import com.google.mlkit.vision.common.InputImage
+
+class ImageAnalyzer(val context: Context) : ImageAnalysis.Analyzer {
+
+    @SuppressLint("UnsafeOptInUsageError")
+    override fun analyze(image: ImageProxy) {
+        val img = image.image
+        if (img != null) {
+            val inputImage = InputImage.fromMediaImage(img, image.imageInfo.rotationDegrees)
+
+            // Process image searching for barcodes
+            val options = BarcodeScannerOptions.Builder()
+                .setBarcodeFormats(
+                    Barcode.FORMAT_QR_CODE,
+                    Barcode.FORMAT_EAN_13,
+                    Barcode.FORMAT_EAN_8,
+                    Barcode.FORMAT_CODE_128,
+                )
+                .build()
+
+            val scanner = BarcodeScanning.getClient(options)
+
+            scanner.process(inputImage)
+                .addOnSuccessListener { barcodes ->
+                    for (barcode in barcodes) {
+                        Toast.makeText(context, barcode.rawValue, Toast.LENGTH_SHORT).show()
+                        Log.d("IMAGE", barcode.rawValue.toString())
+                    }
+                }
+                .addOnFailureListener {
+                    Log.e("ERROR", it.message.toString())
+                }.addOnCompleteListener {
+                    image.close()
+                    img.close()
+                }
+        }
+    }
+}
